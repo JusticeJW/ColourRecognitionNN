466ac62846de275b4fa27e24ae165cf027aeeaa9
JusticeJW <78367175+JusticeJW@users.noreply.github.com>
2021-02-01 11:08:07 -0500

    Scripts

---
+++ NeuralNetScript.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeuralNetScript : MonoBehaviour
+{
+    public class NeuralNetwork
+    {
+        int[] layer;
+        public Layer[] layers;
+        public NeuralNetwork(int[] layercount)
+        {
+            layer = new int[layercount.Length];
+            for (int i = 0; i < layer.Length; i++)
+            {
+                layer[i] = layercount[i];
+            }
+
+            layers = new Layer[layercount.Length - 1];
+
+            for (int i = 0; i < layers.Length; i++)
+            {
+                layers[i] = new Layer(layercount[i], layercount[i + 1]);
+            }
+        }
+
+        public float[] FeedForward(float[] ins)
+        {
+            layers[0].FeedForward(ins);
+
+            for (int i = 1; i < layers.Length; i++)
+            {
+                layers[i].FeedForward(layers[i - 1].outs);
+            }
+
+            return layers[layers.Length - 1].outs;
+        }
+
+        public void BackProp(float[] expected)
+        {
+            for (int i = layers.Length - 1; i >= 0; i--)
+            {
+                if (i == layers.Length - 1)
+                {
+                    layers[i].BackPropOutput(expected);
+                }
+                else
+                {
+                    layers[i].BackPropHidden(layers[i + 1].gamma, layers[i + 1].weights);
+                }
+            }
+
+            for (int i = 0; i < layers.Length; i++)
+            {
+                layers[i].UpdateWeights();
+            }
+        }
+
+        public class Layer
+        {
+            public int numins;
+            public int numouts;
+            public float lr = 0.005f;
+
+            public float[] ins;
+            public float[] outs;
+            public float[,] weights;
+            public float[,] deltaweights;
+            public float[] gamma;
+            public float[] error;
+            public static System.Random random = new System.Random();
+
+            public Layer(int numberofinputs, int numberofoutputs)
+            {
+                numins = numberofinputs;
+                numouts = numberofoutputs;
+
+                ins = new float[numins];
+                outs = new float[numouts];
+                weights = new float[numouts, numins];
+                deltaweights = new float[numouts, numins];
+                gamma = new float[numouts];
+                error = new float[numouts];
+
+                InitializeWeights();
+            }
+
+            public void InitializeWeights()
+            {
+                for (int i = 0; i < numouts; i++)
+                {
+                    for (int j = 0; j < numins; j++)
+                    {
+                        weights[i, j] = (float)random.NextDouble() - 0.5f;
+                    }
+                }
+            }
+
+            public float[] FeedForward(float[] ins)
+            {
+                this.ins = ins;
+
+                for (int i = 0; i < numouts; i++)
+                {
+                    outs[i] = 0;
+                    for (int j = 0; j < numins; j++)
+                    {
+                        outs[i] += ins[j] * weights[i, j];
+                    }
+
+                    outs[i] = (float)Math.Tanh(outs[i]);
+                }
+
+                return outs;
+            }
+
+            public float TanHDer(float value)
+            {
+                return 1 - (value * value);
+            }
+
+            public void BackPropOutput(float[] expected)
+            {
+                for (int i = 0; i < numouts; i++)
+                {
+                    error[i] = outs[i] - expected[i];
+                }
+
+                for (int i = 0; i < numouts; i++)
+                {
+                    gamma[i] = error[i] * TanHDer(outs[i]);
+                }
+
+                for (int i = 0; i < numouts; i++)
+                {
+                    for (int j = 0; j < numins; j++)
+                    {
+                        deltaweights[i, j] = gamma[i] * ins[j];
+                    }
+                }
+            }
+
+            public void BackPropHidden(float[] gammaforward, float[,] weightsforward)
+            {
+                for (int i = 0; i < numouts; i++)
+                {
+                    gamma[i] = 0;
+
+                    for (int j = 0; j < gammaforward.Length; j++)
+                    {
+                        gamma[i] += gammaforward[j] * weightsforward[j, i];
+                    }
+                    gamma[i] *= TanHDer(outs[i]);
+                }
+
+                for (int i = 0; i < numouts; i++)
+                {
+                    for (int j = 0; j < numins; j++)
+                    {
+                        deltaweights[i, j] = gamma[i] * ins[j];
+                    }
+                }
+            }
+
+            public void UpdateWeights()
+            {
+                for (int i = 0; i < numouts; i++)
+                {
+                    for (int j = 0; j < numins; j++)
+                    {
+                        weights[i, j] -= deltaweights[i, j] * lr;
+                    }
+                }
+            }
+        }
+    }
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        Debug.Log("Hello World");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Tester.cs
@@ -0,0 +1,208 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Tester : MonoBehaviour
+{
+    float[] TestFloats = new float[3];
+    int i = 0;
+
+    NeuralNetScript.NeuralNetwork net = new NeuralNetScript.NeuralNetwork(new int[] { 3, 55, 55, 1 });
+
+    public TextMeshProUGUI outputText;
+
+    public void AppleButtonClick()
+    {
+        if(i < 3)
+        {
+            TestFloats[i] = 0;
+            i++;
+        }
+    }
+
+    public void KiwiButtonClick()
+    {
+        if (i < 3)
+        {
+            TestFloats[i] = 1;
+            i++;
+        }
+    }
+
+    public void BannanaButtonClick()
+    {
+        if (i < 3)
+        {
+            TestFloats[i] = 2;
+            i++;
+        }
+    }
+
+    public void RedButtonClick()
+    {
+        if (i < 3)
+        {
+            TestFloats[i] = 3;
+            i++;
+        }
+    }
+
+    public void GreenButtonClick()
+    {
+        if (i < 3)
+        {
+            TestFloats[i] = 4;
+            i++;
+        }
+    }
+
+    public void BlueButtonClick()
+    {
+        if (i < 3)
+        {
+            TestFloats[i] = 5;
+            i++;
+        }
+    }
+
+    public void ClearOutput()
+	{
+        outputText.text = "Output";
+    }
+
+    public void CustomTestButtonClick()
+    {
+        outputText.text += "\n Custom Test Results:";
+        outputText.text += "\n " + net.FeedForward(TestFloats)[0];
+        i = 0;
+    }
+
+    public void CustomBackPropButtonClick()
+    {
+        outputText.text += "\n Custom Test Results:";
+        outputText.text += "\n" + net.FeedForward(TestFloats)[0];
+        foreach (float j in TestFloats)
+        {
+            if (j == 3)
+                net.BackProp(new float[] { 0.3f });
+            if (j == 4)
+                net.BackProp(new float[] { 0.4f });
+            if (j == 5)
+                net.BackProp(new float[] { 0.5f });
+        }
+        i = 0;
+    }
+
+    public void Learn()
+    {
+        for (int i = 0; i < 10000; i++)
+        {
+            net.FeedForward(new float[] { 0, 1, 3 });
+            net.BackProp(new float[] { 0.3f });
+
+            net.FeedForward(new float[] { 0, 1, 4 });
+            net.BackProp(new float[] { 0.4f });
+
+            net.FeedForward(new float[] { 0, 1, 5 });
+            net.BackProp(new float[] { 0.5f });
+
+            net.FeedForward(new float[] { 0, 2, 3 });
+            net.BackProp(new float[] { 0.3f });
+
+            net.FeedForward(new float[] { 0, 2, 4 });
+            net.BackProp(new float[] { 0.4f });
+
+            net.FeedForward(new float[] { 0, 2, 5 });
+            net.BackProp(new float[] { 0.5f });
+
+            net.FeedForward(new float[] { 0, 3, 1 });
+            net.BackProp(new float[] { 0.3f });
+
+            net.FeedForward(new float[] { 0, 3, 2 });
+            net.BackProp(new float[] { 0.3f });
+
+            net.FeedForward(new float[] { 0, 4, 1 });
+            net.BackProp(new float[] { 0.4f });
+
+            net.FeedForward(new float[] { 0, 4, 2 });
+            net.BackProp(new float[] { 0.4f });
+
+            net.FeedForward(new float[] { 0, 5, 1 });
+            net.BackProp(new float[] { 0.5f });
+
+            net.FeedForward(new float[] { 0, 5, 2 });
+            net.BackProp(new float[] { 0.5f });
+
+            net.FeedForward(new float[] { 1, 0, 3 });
+            net.BackProp(new float[] { 0.3f });
+
+            net.FeedForward(new float[] { 1, 0, 4 });
+            net.BackProp(new float[] { 0.4f });
+
+            net.FeedForward(new float[] { 1, 0, 5 });
+            net.BackProp(new float[] { 0.5f });
+
+            net.FeedForward(new float[] { 1, 2, 3 });
+            net.BackProp(new float[] { 0.3f });
+
+            net.FeedForward(new float[] { 1, 2, 4 });
+            net.BackProp(new float[] { 0.4f });
+
+            net.FeedForward(new float[] { 1, 2, 5 });
+            net.BackProp(new float[] { 0.5f });
+
+            net.FeedForward(new float[] { 1, 3, 0 });
+            net.BackProp(new float[] { 0.3f });
+
+            net.FeedForward(new float[] { 1, 3, 2 });
+            net.BackProp(new float[] { 0.3f });
+        }
+    }
+
+    public void Print()
+    {
+        outputText.text += "\n" + net.FeedForward(new float[] { 0, 1, 3 })[0];
+        outputText.text += "\n" + net.FeedForward(new float[] { 0, 1, 4 })[0];
+        outputText.text += "\n" + net.FeedForward(new float[] { 0, 1, 5 })[0];
+        outputText.text += "\n" + net.FeedForward(new float[] { 0, 2, 3 })[0];
+        outputText.text += "\n" + net.FeedForward(new float[] { 0, 2, 4 })[0];
+        outputText.text += "\n" + net.FeedForward(new float[] { 0, 2, 5 })[0];
+        outputText.text += "\n" + net.FeedForward(new float[] { 0, 3, 1 })[0];
+        outputText.text += "\n" + net.FeedForward(new float[] { 0, 3, 2 })[0];
+        outputText.text += "\n" + net.FeedForward(new float[] { 0, 4, 1 })[0];
+        outputText.text += "\n" + net.FeedForward(new float[] { 0, 4, 2 })[0];
+        outputText.text += "\n" + net.FeedForward(new float[] { 0, 5, 1 })[0];
+        outputText.text += "\n" + net.FeedForward(new float[] { 0, 5, 2 })[0];
+        outputText.text += "\n" + net.FeedForward(new float[] { 1, 0, 3 })[0];
+        outputText.text += "\n" + net.FeedForward(new float[] { 1, 0, 4 })[0];
+        outputText.text += "\n" + net.FeedForward(new float[] { 1, 0, 5 })[0];
+        outputText.text += "\n" + net.FeedForward(new float[] { 1, 2, 3 })[0];
+        outputText.text += "\n" + net.FeedForward(new float[] { 1, 2, 4 })[0];
+        outputText.text += "\n" + net.FeedForward(new float[] { 1, 2, 5 })[0];
+        outputText.text += "\n" + net.FeedForward(new float[] { 1, 3, 0 })[0];
+        outputText.text += "\n" + net.FeedForward(new float[] { 1, 3, 2 })[0];
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        /* 
+        apple = 0 kiwi = 1 bannana = 2 red = 3 green = 4 blue = 5
+
+        Tests:
+           net.FeedForward(new float[] {5,0,1} );
+           net.BackProp(new float[] { 0.5f });
+           net.FeedForward(new float[] {5,0,2} );
+           net.BackProp(new float[] { 0.5f });
+        */
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+	}
+
+}
